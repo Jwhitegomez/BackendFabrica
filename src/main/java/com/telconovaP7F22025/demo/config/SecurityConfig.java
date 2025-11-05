1f2bae7165c18c2a599136bfb7e31345bc87f616
Jwhitegomez <jwhite.gomez@udea.edu.co>
2025-11-04 23:07:27 -0500

    fix SonarCloud major issue:Replace generic exceptions with specific library exceptions or a custom exception.

--- src/main/java/com/telconovaP7F22025/demo/config/SecurityConfig.java
+++ src/main/java/com/telconovaP7F22025/demo/config/SecurityConfig.java
@@ -2,10 +2,8 @@ package com.telconovaP7F22025.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
