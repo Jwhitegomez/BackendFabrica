6309b881002ece8f19487ef88ad60c10037f0bcf
Jwhitegomez <jwhite.gomez@udea.edu.co>
2025-11-04 23:46:06 -0500

    Replace this use of System.out by a logger.

--- src/main/java/com/telconovaP7F22025/demo/util/PasswordHashGenerator.java
+++ src/main/java/com/telconovaP7F22025/demo/util/PasswordHashGenerator.java
@@ -1,18 +1,23 @@
 package com.telconovaP7F22025.demo.util;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Small utility to print a BCrypt hash of a given plaintext.
  * Run with Maven: mvn -DskipTests exec:java -Dexec.mainClass=com.telconovaP7F22025.demo.util.PasswordHashGenerator
  */
 public class PasswordHashGenerator {
+
+    private static final Logger logger = LoggerFactory.getLogger(PasswordHashGenerator.class);
+
     public static void main(String[] args) {
         String plain = "secret";
         if (args != null && args.length > 0) {
             plain = args[0];
         }
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode(plain));
+        logger.info(encoder.encode(plain));
     }
 }
